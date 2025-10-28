05f44f71abe720c657c591b93b2cc9f814f91d4b
FadimeYaren <fyarendurmus@gmail.com>
2025-10-28 06:31:42 +0300

    Initial Commit

---
+++ SalahTime/SalahTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalahTimeApp.swift
+//  SalahTime
+//
+//  Created by yaren on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SalahTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
